dcab4d2b5d538260a1eac3611659bced20add9a1
Sochuks <moseschukwumakajang@gmail.com>
2023-02-03 12:13:27 +0100

    Add reg form to html - collect form data - create user object and save - display message

--- user/views.py
+++ user/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-
-# from . models import createUser
-# from . forms import regForm
+from django.shortcuts import render, redirect
+# from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -9,6 +9,21 @@ def index(request):
 
 # Register user view
 def register(request):
+    if request.method == "POST":
+        fname = request.POST['full_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        pass1 = request.POST['pass_one']
+        pass2 = request.POST['pass_two']
+
+        myUser = User.objects.create_user(fname, email, pass1)
+        username = myUser.username
+
+        myUser.save()
+        
+        messages.success(request, f'Hi {username}, your account was created successfully.')
+        return redirect('index')
+
     return render (request, "user/register.html")
 
 # Create user admin view
